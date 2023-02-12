644ea8e38378ed51809e009a956307125f244217
Kokoroou <66433773+Kokoroou@users.noreply.github.com>
2023-02-12 22:39:13 +0700

    Fix file crawler

--- src/crawl/crawler.py
+++ src/crawl/crawler.py
@@ -1,12 +1,40 @@
 import csv
 import os
 from pathlib import Path
-import sys
 
 import pandas as pd
 import requests
-import tqdm
 from bs4 import BeautifulSoup
+
+
+def _find_topic(soup=None):
+    # A part of HTML which store topic of news
+    t = soup.find('ul', class_='breadcrumb')
+
+    # Topic of news (VD: KhoaHoc)
+    topic = (t.find('a').attrs["data-medium"])[5:]
+
+    return topic
+
+
+def _find_date(soup=None):
+    t = soup.find(class_='header-content')
+
+    date = t.find(class_="date").get_text().split(", ")[1]
+
+    return date
+
+
+def _find_header(soup=None):
+    header = soup.find(class_='title-detail').get_text()
+    return header
+
+
+def _find_tag(link):
+    response = requests.get(link)
+    soup = BeautifulSoup(response.content, "html.parser")
+    header = soup.find(class_='title-detail').get_text()
+    return header
 
 
 def get_links(topic_link=None, title_tag='h2'):
@@ -22,36 +50,6 @@ def get_links(topic_link=None, title_tag='h2'):
         print('Exception {} has happened when get links from URL: {}'.format(type(e).__name__, topic_link))
 
 
-def find_topic(soup=None):
-    # A part of HTML which store topic of news
-    t = soup.find('ul', class_='breadcrumb')  
-
-    # Topic of news (VD: KhoaHoc)
-    topic = (t.find('a').attrs["data-medium"])[5:]
-    
-    return topic
-
-
-def find_date(soup=None):
-    t = soup.find(class_='header-content')  
-    
-    date = t.find(class_="date").get_text().split(", ")[1]
-
-    return date
-
-
-def find_header(soup=None):
-    header = soup.find(class_='title-detail').get_text()
-    return header
-
-
-def find_tag(link):
-   response = requests.get(link)
-   soup = BeautifulSoup(response.content, "html.parser")
-   header = soup.find(class_='title-detail').get_text()
-   return header
-
-
 def crawl_data(link=None, data_dir=Path('./data')):
     """Get data from URL and save to file"""
     try:
@@ -62,7 +60,7 @@ def crawl_data(link=None, data_dir=Path('./data')):
         response = requests.get(link)
         soup = BeautifulSoup(response.content, "html.parser")
 
-        topic = find_topic(soup)
+        topic = _find_topic(soup)
 
         # Create new directory if this topic is not crawled
         topic_dir = data_dir / topic
@@ -70,11 +68,11 @@ def crawl_data(link=None, data_dir=Path('./data')):
             os.mkdir(topic_dir)
 
         # Index file which stores URL of all crawled news in the same topic
-        index_filename = '_Index.csv'.format(topic)  
+        index_filename = '_Index.csv'.format(topic)
         index_filepath = topic_dir / index_filename
 
-        date = find_date(soup)
-        #header = find_header(soup)
+        date = _find_date(soup)
+        # header = _find_header(soup)
 
         # Text file which stores content of the news in link
         if not index_filepath.is_file():
@@ -87,7 +85,7 @@ def crawl_data(link=None, data_dir=Path('./data')):
             # Stop if this link is crawled
             for crawled_link in df.iloc[:, 2]:
                 if link == crawled_link:
-                  return None
+                    return None
 
         text_filename = 'news{}.txt'.format(index)
         text_path = topic_dir / text_filename
@@ -99,17 +97,16 @@ def crawl_data(link=None, data_dir=Path('./data')):
 
         # Write content to file
         with open(text_path, 'w', encoding='UTF-8') as f:
-          # Write title of news
-          f.write(soup.find('h1', class_='title-detail').text + '\n\n')
+            # Write title of news
+            f.write(soup.find('h1', class_='title-detail').text + '\n\n')
 
-          # Write description of news
-          f.write(soup.find('p', class_='description').text)
+            # Write description of news
+            f.write(soup.find('p', class_='description').text)
 
-          # Write all content of news
-          contents = soup.findAll('p', class_='Normal')
-          for i in range(0, len(contents) - 1):
-              f.write('\n' + contents[i].text)
+            # Write all content of news
+            contents = soup.findAll('p', class_='Normal')
+            for i in range(0, len(contents) - 1):
+                f.write('\n' + contents[i].text)
 
     except Exception as e:
         print('Exception {} has happened when crawl data from URL: {}'.format(type(e).__name__, link))
-
